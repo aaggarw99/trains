5b2c4dfd9b6e6bb2ae64a0396d5b90ed98f3380a
aaggarw99 <aaggarw99@gmail.com>
2017-02-13 10:57:10 -0600

    update

--- js/crossingOperator.js
+++ js/crossingOperator.js
@@ -4,17 +4,16 @@ var leftSources = ['images/no_lights_left.svg', 'images/right_light_left.svg', "
 var secIndex = 1;
 
 function runAnimation(){
-	
+
 	/*	var lastSwitch = new Date().getTime();
 		var rightCrossing = document.getElementById('rightCrossing');
 		while(new Date().getTime()-lastSwitch<1000){}
-
 			var lightTimer = new Date().getTime();
 			rightCrossing.src="images/right_light_right.svg";
 			while(new Date().getTime()-lightTimer < 500){}
 			rightCrossing.src="images/left_light_right.svg";
 			while(new Date().getTime()-lightTimer < 1000){}
-		
+
 		rightCrossing.src="images/no_lights_right.svg";*/
 	var timerid=window.setInterval(changeImage, 500);
 	var gateUp=window.setTimeout(gatesUp, 10000);
@@ -23,7 +22,7 @@ function runAnimation(){
 
 
 
-	
+
 
 }
 
@@ -34,7 +33,7 @@ function changeImage(){
 		secIndex=1;
 	}
 	document.getElementById('rightCrossing').src=rightSources[secIndex];
-	
+
 }
 
 function gatesUp(){
