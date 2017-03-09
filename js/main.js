14c365d9a0b1c1fcc6a115e89319da466528a83f
Harrison Shapiro <hshapir@ucs.uchicago.edu>
2017-03-09 11:18:21 -0600

    Blur added to loader

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
+	$('body').css("overflow", "hidden");
     setTimeout(function() {
         $('body').addClass('loaded');
+        $('body').css("overflow", "visible");
+        $('body').css("animation", "bodyIn 1s linear");
     }, 3000);
+    setTimeout(function() {
+    	$('body').css("animation", "bodyOut 0.5s linear");
+    }, 2500);
   });
